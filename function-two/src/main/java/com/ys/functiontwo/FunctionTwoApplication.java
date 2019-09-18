489b2d31fcb2543d8a7e89bc787733055e0303ef
dengpeihui <675820697@qq.com>
2019-09-18 21:03:41 +0800

    Initial commit

---
+++ function-two/src/main/java/com/ys/functiontwo/FunctionTwoApplication.java
@@ -0,0 +1,13 @@
+package com.ys.functiontwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunctionTwoApplication.class, args);
+    }
+
+}
---
+++ funtion-one/src/main/java/com/ys/funtionone/FuntionOneApplication.java
@@ -0,0 +1,13 @@
+package com.ys.funtionone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuntionOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuntionOneApplication.class, args);
+    }
+
+}
